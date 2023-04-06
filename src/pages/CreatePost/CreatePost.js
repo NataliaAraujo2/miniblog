9334fb322892c7343ff0caa6b334ff4c64f93281
NataliaAraujo2 <nataliaanog@gmail.com>
2023-04-05 22:08:55 -0300

    Cria a page New Post

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -1,9 +1,57 @@
 import styles from "./CreatePost.module.css";
 
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
+
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setbody] = useState("");
+  const [tags, setTags] = useState("");
+  const [formError, setFormError] = useState("");
+
+  const handleSubimt = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
-      <h1>Create Post</h1>
+      <h2>CREAT POST</h2>
+      <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
+      <label>
+        <span> Title: </span>
+        <input
+          type="text"
+          name="title"
+          required
+          placeholder="Pense num bom título"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+      </label>
+      <label>
+        <span> Image: </span>
+        <input
+          type="text"
+          name="image"
+          required
+          placeholder="Insira uma imagem que represente o seu texto"
+          onChange={(e) => setTitle(e.target.value)}
+          value={image}
+        />
+      </label>
+      <label>
+        <span> Conteúdo: </span>
+        <textarea
+          type="text"
+          name="body"
+          required
+          placeholder="Insira o conteúdo do post"
+          onChange={(e) => setTitle(e.target.value)}
+          value={body}
+        ></textarea>
+      </label>
     </div>
   );
 };
